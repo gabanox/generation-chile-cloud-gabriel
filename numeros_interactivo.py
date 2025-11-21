e220e272c1d94da2c5d0dfc5e2f174b3ef5d48fa
Gabriel Ramirez <gabanox@gmail.com>
2025-11-21 15:44:56 +0000

    agregue un archivo que hace calculos dinamicos

---
+++ numeros_interactivo.py
@@ -0,0 +1,10 @@
+numero1 = float( input("Ingresa el primer número: ") ) 
+
+numero2 = float( input("Ingresa el segundo número: ") )
+
+print(f"La suma de {numero1} y {numero2} es {numero1 + numero2}")
+print(f"La resta de {numero1} y {numero2} es {numero1 - numero2}")
+print(f"La multiplicación de {numero1} y {numero2} es {numero1 * numero2}")
+print(f"La división de {numero1} y {numero2} es {numero1 / numero2}")
+print(f"El módulo de {numero1} y {numero2} es {numero1 % numero2}")
+print(f"{numero1} elevado a la potencia de {numero2} es {numero1 ** numero2}")
