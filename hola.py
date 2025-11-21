ce9957990b833dc8846197691fd76a30986eeea0
Gabriel Ramirez <gabanox@gmail.com>
2025-11-21 14:53:15 +0000

    creacion de archivos de texto y numeros

---
+++ hola.py
@@ -0,0 +1,7 @@
+# Tu nombre
+# Su ciudad 
+# Un emoji
+
+print("Gabriel Ramirez")
+print("Ciudad de Mexico pero ahora estoy en Santiago")
+print("🇨🇱")
---
+++ numeros.py
@@ -0,0 +1,10 @@
+entero  = 10 
+decimal = 3.14 
+suma    = entero + 5
+modulo = entero % 15
+
+print("Suma :", suma)
+print("Decimal :", decimal )
+print("Division:", ( (entero / 3)+2) )
+print("Potencia:", (entero**2) )
+print("Módulo:", modulo)
